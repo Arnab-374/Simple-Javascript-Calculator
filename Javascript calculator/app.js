9540bce1ef93a7410d239eba4771dbc6eef18ee4
Arnab-374 <41272970+Arnab-374@users.noreply.github.com>
2018-07-16 04:26:24 +0530

    Add files via upload

---
+++ Javascript calculator/app.js
@@ -0,0 +1,27 @@
+var box = document.querySelector(".display");
+
+function addToScreen(x ) {
+  box.value += x;
+  if (x == 'c') {
+    box.value = "";
+  }
+}
+
+function answer() {
+  x = box.value;
+  x = eval(x);
+  box.value = x;
+}
+
+function backspace() {
+  var number = box.value;
+  var len = number.length-1;
+  var newNumber = number.substring(0, len);
+  box.value = newNumber;
+}
+
+function power(y) {
+  x = box.value;
+  x = Math.pow(x, y);
+  box.value = x;
+}
